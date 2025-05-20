a922ba52c2c775a6bd93661b31f650c0dbd4db93
taishinanbo <namba@sainta.co.jp>
2025-05-20 19:38:24 +0900

    fix: ファイル名の大文字小文字の不一致を修正

--- frontend/src/FetchService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const fetchService = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5050',
-  withCredentials: true,
-});
-
-export default fetchService;
